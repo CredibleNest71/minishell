58ec311b72a703102a1336d92cad0417127a7b30
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2024-01-08 12:51:43 +0000

    libft added

---
+++ minishell.h
@@ -0,0 +1,12 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "libft/libft.h"
+
+struct s_command {
+	char	**cmd;
+}	typedef t_command;
+
+#endif

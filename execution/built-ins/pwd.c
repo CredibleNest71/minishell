7b9cb3658e2b3d4cead026a16cfadc4aa618f1ba
Ildiko Jasmin Schmutzer <ischmutz@c3r7p1.42vienna.com>
2024-01-17 15:51:40 +0100

    pwd added

--- execution/built-ins/pwd.c
+++ execution/built-ins/pwd.c
@@ -1,18 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 10:32:36 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/01/17 15:20:51 by ischmutz         ###   ########.fr       */
+/*   Created: 2024/01/17 15:21:04 by ischmutz          #+#    #+#             */
+/*   Updated: 2024/01/17 15:50:47 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-void	ft_cd(t_command content, )
+void	ft_pwd()
 {
-	char	*
+	char	*cwd;
+
+	cwd = getenv("PWD");
+	if (!cwd)
+		ft_printf("Current Working Directory Not Found");
+	ft_printf("%s\n", cwd);
 }

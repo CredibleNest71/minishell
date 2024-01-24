177d7bdbbed864b43f75e05c85d470816fbcb1f0
Ildiko Jasmin Schmutzer <ischmutz@c3r7p1.42vienna.com>
2024-01-24 17:48:51 +0100

    modified cd

--- execution/built-ins/export.c
+++ execution/built-ins/export.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 10:32:36 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/01/24 17:47:58 by ischmutz         ###   ########.fr       */
+/*   Created: 2024/01/23 12:34:44 by ischmutz          #+#    #+#             */
+/*   Updated: 2024/01/23 12:35:21 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
-#include <stdio.h>
 
-//relative or absolute path
-//if only cd was called, go to home directory
-//update env after chdir gets executed
-
-void	ft_cd(t_command paths)
+void	export()
 {
-	char	*path;
-	if (paths->args[0]->type == PATH)
-		path = paths->args[0]->arg;
-	else
-		path = getenv("HOME");
-	if (chdir(path) == -1)
-		perror("cd failure:");
+	
 }

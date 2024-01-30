5def1f49d5e6b2dfea29f857b461ba54ea98428b
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2024-01-30 12:37:30 +0000

    committed the struct :*

--- minishell.h
+++ minishell.h
@@ -5,12 +5,30 @@
 # include <stdio.h>
 # include "libft/libft.h"
 
+enum type {
+	CMD = 0,
+	ARG,
+	PATH,
+	NEX,
+	DIR,
+} typedef e_type;
+
+struct s_token {
+	char 	*str;
+	int		type;
+} typedef t_token;
+
 struct s_command {
-	char				*cmd;
-	char				**args;
+	void				*input;
+	void				*output;
+	t_token				*extra;
+	t_token				*cmd;
+	char				**arg;
+	t_token				**args;
 	int					arg_num;
-	char				*nexus;
+	t_token				*nexus;
 	struct s_command 	*next;
 }	typedef t_command;
+
 
 #endif

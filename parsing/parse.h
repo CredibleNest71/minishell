04494dfebf10e4b438675acd3cbf654cbefa8455
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2024-01-31 15:50:50 +0000

    filips responsibility

--- parsing/parse.h
+++ parsing/parse.h
@@ -4,9 +4,10 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-int	is_seperator(char *str);
-t_token	*tokenify(char *str);
-int count_elements(char *);
-char **token_splitter(char *str);
-t_command **commandlistmaker(char *str);
+int			is_char(char c, char *chars);
+int			is_seperator(char *str);
+t_token	    *tokenify(char *str);
+int         count_elements(char *);
+char        **token_splitter(char *str);
+t_command	**commandlistmaker(char *str);
 #endif

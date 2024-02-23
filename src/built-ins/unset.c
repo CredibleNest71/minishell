91675eba5779e7399bf83fecafbdd93cc45531dc
Ildiko Jasmin Schmutzer <ischmutz@c4r2p1.42vienna.com>
2024-02-23 13:46:31 +0100

    basic unset done

--- src/built-ins/unset.c
+++ src/built-ins/unset.c
@@ -6,13 +6,39 @@
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 11:00:47 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/02/09 12:50:22 by ischmutz         ###   ########.fr       */
+/*   Updated: 2024/02/23 13:46:07 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+#include <strings.h>
 
-/* void	ft_unset(t_bigshell *data, int index)
+void	unset_var(t_bigshell *data)
 {
-	exit(0);
-} */
+	o
+}
+
+void	ft_unset(t_bigshell *data)
+{
+	int		len;
+	t_env	*current;
+	t_env	*current_env;
+	
+	if (!data->commands->args)
+		exit(0);
+	current = data->s_env;
+	current_env = data->env;
+	while (data->commands->args)
+	{
+		len = ft_strlen(data->commands->args->str);
+		while (current && current_env)
+		{
+			if (ft_strncmp(data->commands->args->str, current->str, len) == 0)
+				ft_unset(data);
+			if (ft_strncmp(data->commands->args->str, current_env->str, len) == 0)
+				ft_unset(data);
+			current = current->next;
+			current_env  = current_env->next;
+		}
+	}
+}

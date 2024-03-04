78c1a3e8ba2c1c1d2e8cc18a3919150eb4188a2e
Ildiko Jasmin Schmutzer <ischmutz@c3r7p1.42vienna.com>
2024-03-04 18:59:12 +0100

    heredoc is fucked

--- src/execution/heredoc.c
+++ src/execution/heredoc.c
@@ -6,12 +6,28 @@
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:55:27 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/03/04 12:46:39 by ischmutz         ###   ########.fr       */
+/*   Updated: 2024/03/04 18:51:40 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+#include "../parser/parse.h"
 
+char	*delimiter_finder(t_bigshell *data)
+{
+	t_token *tmp_input;
+	char	*delimiter;
+	
+	delimiter = NULL;
+	tmp_input = data->commands->input;
+	while (tmp_input)
+	{
+		if (tmp_input->type == (enum type) HEREDOC)
+			delimiter = tmp_input->str;
+		tmp_input = tmp_input->next;
+	}
+	return (delimiter);
+}
 
 //${SRC_DIR}${EXEC_DIR}heredoc.c\
 //00644 = S_IRUSR | S_IWUSR
@@ -23,22 +39,28 @@
 void	ft_heredoc(t_bigshell *data)
 {
 	char		*lineread;
+	char		*eof;
 	int			heredoc_fd;
 
 	lineread = NULL;
+	eof = delimiter_finder(data);
+	/* if (!eof)
+		simple_error(data, 1); */
 	heredoc_fd = open("tmpfile.txt", O_CREAT | O_TRUNC | O_RDWR, 00644);
 	if (heredoc_fd == -1)
 		simple_error(data, 1);
 	while (1)
 	{
 		lineread = readline("> ");
-		if (lineread == data->commands->heredoc->str || lineread == NULL)
-			break ;
+		if (lineread == eof || lineread == NULL)
+		 	break ;
 		//have to check for expansions and access that
 		//ex. cat << heredoc EOF
 		//> print the path: $path
 		//EOF
+		lineread = expand(lineread);
 		write(heredoc_fd, lineread, ft_strlen(lineread));
+		//printf("%s\n", lineread);
 	}
 	//pass tmpfile.txt to execution
 	//after execution check for tmpfile and delete it

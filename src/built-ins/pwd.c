624d7a79785dc218db92c7bb4f0f21005c996e2b
Ildiko Jasmin Schmutzer <ischmutz@c2r5p5.42vienna.com>
2024-03-12 11:21:25 +0100

    changed pwd to print data->env. need to update pwd in data->env in cd

--- src/built-ins/pwd.c
+++ src/built-ins/pwd.c
@@ -6,22 +6,48 @@
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:21:04 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/03/01 13:29:51 by ischmutz         ###   ########.fr       */
+/*   Updated: 2024/03/12 11:20:18 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
+// void	ft_pwd(t_bigshell *data)
+// {
+// 	char	*cwd;
+
+// 	cwd = getenv("PWD");
+// 	if (!cwd)
+// 	{
+// 		ft_putstr_fd("Current Working Directory Not Found", 1);
+// 		data->exit_stat = 1;
+// 		exit(data->exit_stat);
+// 	}
+// 	ft_putstr_fd(cwd, 1);
+// 	ft_putchar_fd('\n', 1);
+// 	return ;
+// }
+
 void	ft_pwd(t_bigshell *data)
 {
 	char	*cwd;
+	t_env	*tmp;
 
-	cwd = getenv("PWD");
+	tmp = data->env;
+	while (tmp)
+	{
+		if (ft_strncmp(tmp->str, "PWD", 3))
+		{
+			cwd = tmp->str;
+			break ;
+		}
+		tmp = tmp->next;
+	}
 	if (!cwd)
 	{
 		ft_putstr_fd("Current Working Directory Not Found", 1);
 		data->exit_stat = 1;
-		exit(data->exit_stat);
+		return ;
 	}
 	ft_putstr_fd(cwd, 1);
 	ft_putchar_fd('\n', 1);

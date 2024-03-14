b9400a529cf646dcbaf0b60a104146af229ce538
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2024-03-14 11:33:46 +0000

    echo again

--- src/built-ins/echo.c
+++ src/built-ins/echo.c
@@ -19,61 +19,14 @@ void	ft_echo(t_token *args, int option)
 	int	count;
 
 	count = 0;
-	if (option == 1)
+	while (args)
 	{
-		args = args->next;
-		if (!args)
-			return ;
-		while (args)
-		{			
-			if (count++)
-				write(1, " ", 1);
-			ft_putstr_fd(args->str, 1);
-			args = args->next;	
-		}
-	}
-	else
-	{
-		if (!args)
-		{
-			ft_putchar_fd('\n', 1);
-			return ;
-		}
-		while(args)
-		{
-			if (count++)
-				write(1, " ", 1);
-			ft_putstr_fd(args->str, 1);
+		if (count++)
+			ft_putchar_fd(' ', 1);
+		ft_putstr_fd(args->str, 1);
 		args = args->next;
 	}
 	if (!option)
 		ft_putchar_fd('\n', 1);
-	// if (option == 1)
-	// {
-	// 	args = args->next;
-	// 	if (!args)
-	// 		return ;
-	// 	while (args)
-	// 	{
-	// 		ft_putstr_fd(args->str, 1);
-	// 		args = args->next;	
-	// 	}
-	// }
-	// else
-	// {
-	// 	if (!args)
-	// 	{
-	// 		ft_putchar_fd('\n', 1);
-	// 		exit(0);
-	// 	}
-	// 	while(args)
-	// 	{
-	// 		if (count++)
-	// 			write(1, " ", 1);
-	// 		ft_putstr_fd(args->str, 1);
-	// 		args = args->next;
-	// 	}
-	// 	ft_putchar_fd('\n', 1);
-	// }
 	return ;
 }

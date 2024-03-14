ac12ef461d7861df164fc9517de98053463a00d4
CredibleNest71 <144441212+CredibleNest71@users.noreply.github.com>
2024-03-14 11:34:40 +0000

    no longer printing echo flag -n

--- src/built-ins/echo.c
+++ src/built-ins/echo.c
@@ -23,7 +23,8 @@ void	ft_echo(t_token *args, int option)
 	{
 		if (count++)
 			ft_putchar_fd(' ', 1);
-		ft_putstr_fd(args->str, 1);
+		if (strncmp(args->str, "-n", 2)
+			ft_putstr_fd(args->str, 1);
 		args = args->next;
 	}
 	if (!option)

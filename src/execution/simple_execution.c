57936db21b19b48444950ef9a793aaef5e96dd4f
Ildiko Jasmin Schmutzer <ischmutz@c2r5p5.42vienna.com>
2024-03-15 18:02:27 +0100

    addded fork to fake main

--- src/execution/simple_execution.c
+++ src/execution/simple_execution.c
@@ -6,7 +6,7 @@
 /*   By: ischmutz <ischmutz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 17:54:30 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/03/14 11:22:39 by ischmutz         ###   ########.fr       */
+/*   Updated: 2024/03/15 17:59:34 by ischmutz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,6 +188,12 @@ int	main(int argc, char **argv, char **env)
 		if (builtin_allrounder(&data) == 0)
 			continue ;
 		//print_cmds(data.commands);
-		simple_exec(&data);
+		if (data.num_cmd == 1)
+		{
+			if ((data.id = fork()) == -1)
+				fatal_error(&data, 1);
+			if (data.id == 0)
+				simple_exec(&data);
+		}
 	}
 }

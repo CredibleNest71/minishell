0e877b7ed1429026127750d745b0913098bc969e
CredibleNest71 <moritz.res@gmail.com>
2024-04-08 11:39:37 +0200

    changeds

--- src/main/sig.c
+++ src/main/sig.c
@@ -11,7 +11,7 @@ int	check_sigs()
 	return (g_sig.sigint + g_sig.sigquit * 10);
 }
 
-void	redo_rl()
+static void	redo_rl()
 {
 	write(1, "\n", 1);
 	rl_on_new_line();
@@ -19,10 +19,20 @@ void	redo_rl()
 	rl_redisplay();
 }
 
-void	set_global()
+static void	set_global()
 {
 	g_sig.sigint = 1;
 }
+// static void    signal_processing3(int sig)
+// {
+//     if (sig == SIGINT)
+//     {
+//         ioctl(0, TIOCSTI, "\n");
+//         g_sigint = SIGINT;
+//         rl_replace_line("", 0);
+//         rl_on_new_line();
+//     }
+// }
 
 void	set_signals(int mode)
 {
@@ -35,5 +45,10 @@ void	set_signals(int mode)
 	{
 		signal(SIGINT, &set_global);
 		signal(SIGQUIT, SIG_DFL);
+	}	
+	else if (mode == 1)
+	{
+		signal(SIGINT, &set_global);
+		signal(SIGQUIT, SIG_IGN);
 	}
 }

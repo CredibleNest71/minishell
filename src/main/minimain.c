825ac69fb149331cb4f244137d491134cf850696
CredibleNest71 <moritz.res@gmail.com>
2024-05-11 14:38:21 +0200

    minor stuff

--- src/main/minimain.c
+++ src/main/minimain.c
@@ -6,7 +6,7 @@
 /*   By: mresch <mresch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:33:48 by ischmutz          #+#    #+#             */
-/*   Updated: 2024/05/11 14:18:03 by mresch           ###   ########.fr       */
+/*   Updated: 2024/05/11 14:29:56 by mresch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,17 @@ int	mainloop(t_bigshell *data, char *lineread)
 	{
 		in = get_input(data, lineread);
 		if (in == -1)
-			return (write(1, "exit\n", 6), 1);
+			return (/*write(1, "exit\n", 6),*/ 1);
 		else if (!in)
 			continue ;
 		store_restore_fds(data, 1);
 		ft_heredoc(data);
+		set_signals(1);
 		if (g_sig == SIGINT)
 		{
 			make_over(data, lineread);
 			continue ;
 		}
-		set_signals(1);
 		if (!execute(data))
 			continue ;
 		make_over(data, lineread);
